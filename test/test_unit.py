c7bebda1bbf517930fca221e26d3d924109a9465
Erik Marques <lucroalternativo@outlook.com>
2025-03-24 11:33:03 -0300

    Correção, aprimoramento e adição de novos testes

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,3 +1,4 @@
+import pytest
 from pathlib import Path
 from pandas import DataFrame
 
@@ -5,24 +6,26 @@ from pandas import DataFrame
 def test_dados_excel():
     from cadastro import DADOS_EXCEL
 
-    assert DADOS_EXCEL.as_posix() == Path(r'C:\Users\Estudos\Documents\cadastro_sec\dados\abastecimento_2024.xlsx').as_posix()
+    assert DADOS_EXCEL.as_posix() == Path().cwd().joinpath('dados', 'abastecimento_2024.xlsx').as_posix()
 
 def test_output_excel():
     from cadastro import DADOS_OUTPUT
 
-    assert DADOS_OUTPUT.as_posix() == Path(r'C:\Users\Estudos\Documents\cadastro_sec\dados\cadastros.xlsx').as_posix()
+    assert DADOS_OUTPUT.as_posix() == Path().cwd().joinpath('dados', 'cadastros.xlsx').as_posix()
 
+# @pytest.mark.skip()
 def test_extrair_dados():
     from cadastro import extrair_dados
 
     dados = extrair_dados()
     
     assert isinstance(dados, DataFrame), 'Os dados extraídos não são um DataFrame'
-    assert dados.empty is True, 'Os dados extraídos estão vazios'
+    assert dados.empty is False, 'Os dados extraídos estão vazios'
 
+# @pytest.mark.skip()
 def test_extrair_dados_completos():
     from cadastro import extrair_dados_completos
 
     dados = extrair_dados_completos()
 
-    assert dados.notna().all().all() is True, 'Ainda existem dados faltando'
+    assert dados.notna().all().all(), 'Ainda existem dados faltando'
