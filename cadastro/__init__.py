6541a268f476b38df625c1b0341cb39c801b8b92
Erik Marques <lucroalternativo@outlook.com>
2025-03-24 09:15:41 -0300

    Alterada ordem de inicialização

--- cadastro/__init__.py
+++ cadastro/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from extrair_dados import extrair_dados
+from extrair_dados import extrair_dados, extrair_dados_completos
 
 DADOS_EXCEL = Path(__file__).parents[1].joinpath('dados', 'abastecimento_2024.xlsx')
 DADOS_OUTPUT = DADOS_EXCEL.parent.joinpath('cadastros.xlsx')
