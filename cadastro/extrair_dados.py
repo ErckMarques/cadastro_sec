9a10cbb26fda889abce6e20b8f6d6704297fb8ff
Erik Marques <lucroalternativo@outlook.com>
2025-03-24 12:12:45 -0300

    Alterada funções de extração de dados

--- cadastro/extrair_dados.py
+++ cadastro/extrair_dados.py
@@ -1,29 +1,41 @@
 from pandas import concat, DataFrame, read_excel
 
-from cadastro import DADOS_EXCEL, MESES
+from cadastro import DADOS_EXCEL, DADOS_OUTPUT, MESES
 
 _cadastros: DataFrame | None = None
+_cadastros_completos = DataFrame | None = None
+_header_sheets = ['NOME ', 'APELIDO ', 'ENDEREÇO', 'REFERENCIA', 'CPF', 'TELEFONE']
 
 def extrair_dados() -> DataFrame:
     global _cadastros
-    
+    global _header_sheets
+
     if _cadastros is not None:
         return _cadastros
     
     aux = list()
-    header_sheets = ['NOME ', 'APELIDO ', 'ENDEREÇO', 'REFERENCIA', 'CPF', 'TELEFONE']
     for mes in MESES:
-        df = read_excel(DADOS_EXCEL, sheet_name=mes, header=6, usecols=header_sheets, names=header_sheets)
+        df = read_excel(DADOS_EXCEL, sheet_name=mes, header=6, usecols=_header_sheets, names=_header_sheets)
         aux.append(df)
 
     _cadastros = concat(aux, ignore_index=True)
     
     return _cadastros
 
-def extrair_dados_completos():
+def extrair_dados_completos(export: bool = True) -> DataFrame:
     global _cadastros
+    global _cadastros_completos
+    global _header_sheets
 
     if _cadastros is not None:
         extrair_dados()
+    
+    _cadastros_completos = _cadastros.dropna()
+    
+    if export:
+        _cadastros_completos.to_excel(DADOS_OUTPUT, sheet_name='CADASTRO_GERAL', startrow=2, index=False)
 
-    return _cadastros.dropna()
+    return _cadastros_completos
+
+    
+    
