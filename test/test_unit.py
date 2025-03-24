b817b7840d7e0ab45fa51f2a83673554c52e007f
Erik Marques <lucro.alternativo@outlook.com>
2025-03-24 15:31:54 -0300

    modificada estrutura do arquivo

--- test/test_unit.py
+++ test/test_unit.py
@@ -4,29 +4,24 @@ from pandas import DataFrame
 
 
 from cadastro import extrair_dados, extrair_dados_completos
+from cadastro import DADOS_EXCEL, DADOS_OUTPUT
 
 def test_dados_excel():
-    from cadastro import DADOS_EXCEL
 
     assert DADOS_EXCEL.as_posix() == Path().cwd().joinpath('dados', 'abastecimento_2024.xlsx').as_posix()
 
 def test_output_excel():
-    from cadastro import DADOS_OUTPUT
 
     assert DADOS_OUTPUT.as_posix() == Path().cwd().joinpath('dados', 'cadastros.xlsx').as_posix()
 
-# @pytest.mark.skip()
 def test_extrair_dados():
-    from cadastro import extrair_dados
 
     dados = extrair_dados()
     
     assert isinstance(dados, DataFrame), 'Os dados extraídos não são um DataFrame'
     assert not dados.empty, 'o Dataframe retornado está vazio'
 
-# @pytest.mark.skip()
 def test_extrair_dados_completos():
-    from cadastro import extrair_dados_completos
 
     dados = extrair_dados_completos()
 
