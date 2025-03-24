33ff8715b6e9790c118d7ee79a515d9fce62f222
Erik Marques <lucroalternativo@outlook.com>
2025-03-24 09:19:26 -0300

    Alterada ordem de inicialização

--- cadastro/__init__.py
+++ cadastro/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from extrair_dados import extrair_dados, extrair_dados_completos
-
 DADOS_EXCEL = Path(__file__).parents[1].joinpath('dados', 'abastecimento_2024.xlsx')
 DADOS_OUTPUT = DADOS_EXCEL.parent.joinpath('cadastros.xlsx')
 MESES = 'JANEIRO,FEVEREIRO,MARÇO,ABRIL,MAIO,JUNHO,JULHO,AGOSTO,SETEMBRO,OUTUBRO,NOVEMBRO,DEZEMBRO'.split(',')
+
+from extrair_dados import extrair_dados, extrair_dados_completos
