71ed86ca194d188b928bd62a375912f5741564b0
Erik Marques <lucro.alternativo@outlook.com>
2025-03-24 15:14:08 -0300

    Melhorias nos testes

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,4 +1,7 @@
+from pandas import DataFrame
 from pathlib import Path
+
+from cadastro import extrair_dados, extrair_dados_completos
 
 def test_dados_excel():
     from cadastro import DADOS_EXCEL
@@ -11,11 +14,16 @@ def test_output_excel():
     assert DADOS_OUTPUT.as_posix() == Path(r'C:\Users\Estudos\Documents\cadastro_sec\dados\cadastros.xlsx').as_posix()
 
 def test_extrair_dados():
-    from cadastro import extrair_dados
-    from pandas import DataFrame
 
     dados = extrair_dados()
     
     assert isinstance(dados, DataFrame), 'Os dados extraídos não são um DataFrame'
-    assert dados.shape[0] > 0, 'Os dados extraídos estão vazios'
+    assert not dados.empty, 'Os dados extraídos estão vazios'
+
+def test_extrair_dados_completos():
+
+    dados = extrair_dados_completos()
+    
+    assert dados.notna().all().all(), 'Os dados extraídos estão com valores nulos'
+
 
