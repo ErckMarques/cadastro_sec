d37d62b719f93a0123e1901607b784585802a4f5
Erik Marques <lucro.alternativo@outlook.com>
2025-03-24 15:15:24 -0300

    alterações em extrai_dados

--- cadastro/extrair_dados.py
+++ cadastro/extrair_dados.py
@@ -4,7 +4,14 @@ from cadastro import DADOS_EXCEL, MESES
 
 _cadastros: DataFrame | None = None
 
+def _normalizar_dados(dados: DataFrame) -> None:
+    '''Esta função normaliza os dados de cadastro, deixando todo o texto em maiúsculo e removendo espaços em branco.'''
+    for coluna in dados.columns:
+        if dados[coluna].dtype == 'object':
+            dados[coluna] = dados[coluna].str.upper().str.strip()
+
 def extrair_dados() -> DataFrame:
+    '''Esta função extrai os dados de cadastro de todos os meses e retorna um DataFrame com todos os dados.'''
     global _cadastros
     
     if _cadastros is not None:
@@ -13,12 +20,19 @@ def extrair_dados() -> DataFrame:
     aux = list()
     header_sheets = ['NOME ', 'APELIDO ', 'ENDEREÇO', 'REFERENCIA', 'CPF', 'TELEFONE']
     for mes in MESES:
-        df = read_excel(DADOS_EXCEL, sheet_name=mes, header=6, usecols='A:F', names=header_sheets)
+        df = read_excel(DADOS_EXCEL, sheet_name=mes, header=6, usecols=header_sheets, names=header_sheets)
         aux.append(df)
 
     _cadastros = concat(aux, ignore_index=True)
+    _normalizar_dados(_cadastros)
     
     return _cadastros
 
-cad_completos = _cadastros.dropna()
-cad_completos.to_excel(DADOS_EXCEL.parent.joinpath('cadastros.xlsx'), 'CADASTRO_GERAL',startrow=2, index=False)
+def extrair_dados_completos() -> DataFrame:
+    '''Esta função retorna os dados de cadastro que possuem todos os campos preenchidos. Ela remove linhas com campos vazios.'''
+    global _cadastros
+    
+    if _cadastros is None:
+        extrair_dados()
+    
+    return _cadastros.dropna()
