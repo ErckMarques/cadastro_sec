2c480a79e73ff035f70cf48d7d4f439eb74dd432
Erik Marques <lucro.alternativo@outlook.com>
2025-03-26 01:12:04 -0300

    teste para a funcionalidade da classe de normalização de dados

--- test/test_normalizar.py
+++ test/test_normalizar.py
@@ -1,17 +1,52 @@
 import pytest
 
+from pandas import DataFrame
+from numpy import nan
+
 from cadastro import NormalizarXl
-from cadastro import ExtratorDadosCadastro
 
-@pytest.fixture(scope='module')
-def dados_completos():
-    extrator = ExtratorDadosCadastro()
-    extrator.extrair_dados_completos()
-    return extrator.cadastros_completos
+@pytest.fixture
+def dados():
+    header_sheets = ['NOME', 'APELIDO', 'ENDEREÇO', 'REFERENCIA', 'CPF', 'TELEFONE']
+    return DataFrame(
+        {
+            1: '''RONALDO JOSÉ MARIANO
+            LUANNE DA SILVA MARTINS
+            MÁRCIO AGUIAR DA SILVA
+            '''.split(),
+            2: [nan, nan, nan],
+            3: '''SÍTIO AGOSTINHO
+            SITIO CACHOEIRA DO SALOBRO
+            SITIO TANQUE VERDE
+            '''.split(),
+            4: '''PROX A FÁBRICA DE VENENO
+            AO LADO DO CAMPO
+            PROX AO SIT DE PEDRO CABRAL NO CASARÃO
+            '''.split(),
+            5: '''117.527.524-73
+            131.411.584-79
+            833.126.094-53
+            '''.split(),
+            6:'''99282-2899
+            99571-8384
+            98972-0778
+            '''.split()
+        }, columns=header_sheets
+    )
 
-@pytest.fixture(scope='module')
-def normalizador(dados_completos):
-    return NormalizarXl(dados_completos)
+@pytest.fixture
+def norm(dados):
+    return NormalizarXl(dados)
 
-def test_normalizar_telefone():
-    pass
+def test_todas_normalizacoes(norm: NormalizarXl):
+    norm.normalizar()
+
+    tratados = norm.dados_normalizados
+
+    assert tratados['APELIDO'].notna().all(), 'Há dados faltantes na coluna APELIDOS'
+    assert tratados['ENDEREÇO'].str.match(), 'Há ENDEREÇOS inconsistentes com o padrão estabelecido'
+    # assert tratados['REFERENCIA']
+    assert tratados['CPF'].str.match().all(), 'Há CPFs inválidos'
+    assert tratados['TELEFONE'].str.match().all(), 'Há TELEFONES inválidos'
+
+
