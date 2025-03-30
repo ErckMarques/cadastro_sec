26e9ac016fca5a6982a9726a56621c4eda91972c
Erik Marques <lucro.alternativo@outlook.com>
2025-03-30 00:55:16 -0300

    Add logs e melhorias para o algoritmo de cadastramento

--- cadastro/cadastrar.py
+++ cadastro/cadastrar.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
 
 from cadastro import DADOS_SITE
@@ -16,17 +17,19 @@ from cadastro._log import log
 class Cadastrar:
     def __init__(self, dados_cadastro: DataFrame):
         self._dados_cadastro = dados_cadastro
-        self._driver = webdriver.Chrome(ChromeDriverManager().install())
+        self._driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self._wait = WebDriverWait(self._driver, 10)
 
     def logar(self):
         self._driver.get(DADOS_SITE.get('URL'))
         log.info('Acessando site do Sistema da Secretaria de Agricultura')
         log.info('Logando no sistema...Aguarde')
-        inputs = self._wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'input')))
+        field_user = self._wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, DADOS_SITE.get('FIELD_USER'))))
+        field_pwd = self._wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, DADOS_SITE.get('FIELD_PWD'))))
         log.info('Preenchendo usuário e senha')
-        inputs[0].send_keys(DADOS_SITE.get('USUARIO')) 
-        inputs[1].send_keys(DADOS_SITE.get('SENHA'))
+        field_user.send_keys(DADOS_SITE.get('USUARIO'))
+        field_pwd.send_keys(DADOS_SITE.get('SENHA'))
+
         log.info('Logando no sistema')
         self._driver.find_element(By.TAG_NAME, 'button').click()
         if self._wait.until(EC.presence_of_element_located((By.CLASS_NAME, DADOS_SITE.get('GRAFICO')))):
@@ -36,13 +39,11 @@ class Cadastrar:
         self._driver.quit()
 
 
-    def cadastrar(self, nome, email):
+    def cadastrar(self):
         log.info('Abrindo página de cadastro')
-        menu_lat = self._wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, DADOS_SITE.get('MENU'))))
-        for item in menu_lat:
-            if item.get_property('alt') == 'Cadastro':
-                item.click()
-                break
+        menu_cad = self._wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, DADOS_SITE.get('CAD'))))
+        not_cadastrados = list()
+        
         if self._wait.until(EC.presence_of_element_located((By.TAG_NAME, 'input'))):
             
             log.info('Identificando Campos do formulário de Cadastro')
@@ -64,22 +65,26 @@ class Cadastrar:
                 else:
                     log.error(f'Erro ao cadastrar {row.NOME}')
 
-    def _loc_campos(self) -> dict[str, WebElement]:
+    def _loc_campos(self) -> dict[str, WebElement | Select]:
         """Localiza e retorna um dicionário de elementos web (input, select, button) com chaves específicas."""
         web_elements = {}
-        
+        log.info('Capturando referencia dos elementos da ficha cadastral')
         # Captura inputs cujo atributo 'name' está em DADOS_SITE
         for element in self._wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'input'))):
             if (name := element.get_attribute('name')) in DADOS_SITE.values():
+                log.info(f'Pegando referencia do elemento: {name}')
                 web_elements[name] = element
         
+        # adiciona o elemento 'outros'
+        log.info('Pegando referencia de Outros e Localidade.')
+        web_elements['outros'] = self._wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, DADOS_SITE.get('CLASS'))))
         # Adiciona o elemento 'select' com a chave 'local'
         web_elements['local'] = Select(self._wait.until(EC.presence_of_element_located((By.TAG_NAME, 'select'))))
-        
+
         # Adiciona o último botão encontrado com a chave 'botao'
         botoes = self._driver.find_elements(By.TAG_NAME, 'button')
         for btn in botoes:
-            if btn.get_attribute('text') == 'Avançar':
+            if btn.text == 'Avançar':
                 web_elements['btn'] = btn
         
         return web_elements
