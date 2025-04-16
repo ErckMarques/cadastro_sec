4be71ba75ea3b60fa742f8e61bea119f4e502ca5
Erik Marques <lucro.alternativo@outlook.com>
2025-04-16 20:07:19 -0300

    Tentativa de adicionar configuração do Handler para modo de dev

--- cadastro/_log/__init__.py
+++ cadastro/_log/__init__.py
@@ -1,3 +1,8 @@
 import logging as log
 
+from cadastro import ENV_VARS
+
+if ENV_VARS.get('MODE') == 'dev':
+    log.basicConfig(level=log.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 log.basicConfig(level=log.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
